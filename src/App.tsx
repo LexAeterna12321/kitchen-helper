97fced574bc43c288ddfb5874373869f6f418f12
Lex Aeterna <lexaeterna12321@gmail.com>
2019-03-25 23:17:06 +0100

    typescript initial configuration added

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import "./App.css";
+
+class App extends Component {
+  render(): JSX.Element {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Hello from Typescript</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
