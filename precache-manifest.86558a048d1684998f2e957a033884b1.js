5f09e0f3ec53756b226e140aec3eb91947f86a40
Lex Aeterna <lexaeterna12321@gmail.com>
2019-04-09 23:06:36 +0200

    Updates

---
+++ precache-manifest.86558a048d1684998f2e957a033884b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6af6d485edb6cfb66b80",
+    "url": "/kitchen-helper/static/css/main.c073e9ee.chunk.css"
+  },
+  {
+    "revision": "6af6d485edb6cfb66b80",
+    "url": "/kitchen-helper/static/js/main.dccc5d15.chunk.js"
+  },
+  {
+    "revision": "3f7d3d6a362c46a607d6",
+    "url": "/kitchen-helper/static/js/runtime~main.bd59581d.js"
+  },
+  {
+    "revision": "d069c7a4b601fcffbaa9",
+    "url": "/kitchen-helper/static/js/2.2fc49a87.chunk.js"
+  },
+  {
+    "revision": "37094f5d9980d68132cc7c29ad7e809c",
+    "url": "/kitchen-helper/index.html"
+  }
+];
